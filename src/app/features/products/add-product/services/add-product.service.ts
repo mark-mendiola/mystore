619f3ca7ab1268e8a74e7cbe8c0234ea99734fab
Mark Anthony Mendiola <Mark.Mendiola@nowcom.com.ph>
2019-05-28 13:08:23 +0800

    Updated add-product.service.ts

--- src/app/features/products/add-product/services/add-product.service.ts
+++ src/app/features/products/add-product/services/add-product.service.ts
@@ -16,8 +16,8 @@ const httpOptions = {
 export class AddProductService {
 
   requestUrl = {
-    products: 'https://my-json-server.typicode.com/mark-mendiola/mystore/products',
-    categories: 'https://my-json-server.typicode.com/mark-mendiola/mystore/product_categories',
+    products: 'http://my-json-server.typicode.com/mark-mendiola/mystore/products',
+    categories: 'http://my-json-server.typicode.com/mark-mendiola/mystore/product_categories',
   };
 
   constructor(
